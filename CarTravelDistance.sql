312267f48fe80d9fa3229264f655d0875d2c16e0
unknown <kiran.om@gmail.com>
2023-09-06 23:40:25 +0100

    Table created and sample data inserted

--- CarTravelDistance.sql
+++ CarTravelDistance.sql
@@ -1,2 +1,11 @@
 /* Program calculating how much distance travelled by each car per day */
 
+CREATE TABLE tblCarTravelDistance (
+    car varchar(80),
+    day varchar(50),
+    cumulative_distance integer
+)
+;
+
+INSERT INTO tblCarTravelDistance VALUES
+    ('Creta', 'Day1', 50);
