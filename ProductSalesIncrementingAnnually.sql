aa7793bbb591129e978c5df6188e0914252cabde
unknown <kiran.om@gmail.com>
2023-09-25 22:11:43 +0100

    Product (Mithai-Sweets) data

--- ProductSalesIncrementingAnnually.sql
+++ ProductSalesIncrementingAnnually.sql
@@ -6,3 +6,20 @@ CREATE TABLE tblProductSalesAmount (
     , year_of_sales character varying(10)
 )
 ;
+
+INSERT INTO tblProductSalesAmount VALUES
+    ('Barfi', 450, 2019)
+  , ('Barfi', 475, 2020)
+  , ('Barfi', 462, 2021)
+  , ('Barfi', 445, 2022)
+  , ('Peda', 400, 2019)
+  , ('Peda', 505, 2020)
+  , ('Peda', 356, 2021)
+  , ('Peda', 278, 2022)
+  , ('Sheera', 305, 2019)
+  , ('Sheera', 389, 2020)
+  , ('Sheera', 520, 2021)
+  , ('Sheera', 572, 2022)
+  , ('Sheera', 594, 2023)
+  ;
+
