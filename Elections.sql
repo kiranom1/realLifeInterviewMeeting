51cfdb1e1b575e5c558cb0fd9941bd99e95a6101
unknown <kiran.om@gmail.com>
2023-10-22 18:57:14 +0100

    Sunday 22.October.2023 18:57:14

--- Elections.sql
+++ Elections.sql
@@ -24,3 +24,30 @@ INSERT INTO "tblCandidates" ("gender", "age", "party") VALUES
   , ('F', 57, 'L')
 ;
 
+CREATE TABLE IF NOT EXISTS "tblResults"
+(
+    "constituency_id" integer
+  , "candidate_id" integer
+  , "votes" integer
+  , constraint "fk_candidate_id" FOREIGN KEY("candidate_id") REFERENCES "tblCandidates"("id")
+)
+;
+
+INSERT INTO "tblResults" VALUES
+    (11, 101, 847530)
+  , (11, 104, 283409)
+  , (22, 102, 293841)
+  , (22, 105, 394385)
+  , (33, 103, 429084)
+  , (33, 106, 303890)
+  ;
+
+SELECT *
+FROM "tblCandidates" AS "tc"
+JOIN "tblResults" AS "tr" ON "tc"."id" = "tr"."candidate_id"
+;
+
+SELECT "tc"."party", "tr"."constituency_id", "tr"."candidate_id", "tr"."votes"
+FROM "tblCandidates" AS "tc"
+JOIN "tblResults" AS "tr" ON "tr"."candidate_id" = "tc"."id"
+;
